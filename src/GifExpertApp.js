545fbce6c1492294f4bf761e413559bd080bdc82
PabloChacon1997 <47465321+PabloChacon1997@users.noreply.github.com>
2020-07-17 15:57:42 -0500

    First Commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GiftGrid } from './components/GiftGrid';
+
+const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch']);
+    
+    return (  
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr />
+            <ol>
+                {
+                    categories.map( category =>(
+                        <GiftGrid
+                            key={category} 
+                            categoria={category} 
+                        />
+                    ))
+                }
+            </ol>
+        </>
+    );
+}
+ 
+export default GifExpertApp;
