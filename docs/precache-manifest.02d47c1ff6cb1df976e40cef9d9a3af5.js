5c67a12a09668041d09b51ffd24c380f6c878581
PabloChacon1997 <47465321+PabloChacon1997@users.noreply.github.com>
2020-07-17 15:59:33 -0500

    Docs agregado

---
+++ docs/precache-manifest.02d47c1ff6cb1df976e40cef9d9a3af5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a21e4b4fe5c85833a4f88e87a49a9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd21e6f462ebd20115c6",
+    "url": "/static/css/main.29ffd6de.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd21e6f462ebd20115c6",
+    "url": "/static/js/main.1d3387e5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
